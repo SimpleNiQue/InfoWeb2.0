3c9f6b3bac8f4824ae93b7b0a3c8cc20161b4f85
SimpleNiQue <simplenick01@gmail.com>
2023-02-27 11:50:27 +0100

    Tailwind Setup and first template

---
+++ tailwind-demo/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        "nick-bg": "#15074e",
+        "my-white": "ffffff",
+        "less-purple": "#372583",
+        "sly-blue": "DAE8FC"
+      }
+    },
+  },
+  plugins: [],
+}
